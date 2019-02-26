9ef6e8e35c6014602f1e2e1b3ca9d4ef80ff2fd3
Alvin Veroy <locusts.org@gmail.com>
2019-02-26 13:49:10 +0800

    Adding Heroku vars test fullfillment for issue #2

--- test/heroku.js
+++ test/heroku.js
@@ -1,3 +1,14 @@
+/*
+Heroku config vars can be access through process.env.<config key>
+To make this script work, include the Procfile in the root directory that contains
+
+web:    node ./test/heroku.js
+
+then push it in Heroku using heroku-cli.
+make sure to create a config vars name test_vars and test_vars2 and add some random values on it.
+To run, type heroku run web
+*/
+
 var heroku_env1 = process.env.test_vars;
 var heroku_env2 = process.env.test_vars2;
 
